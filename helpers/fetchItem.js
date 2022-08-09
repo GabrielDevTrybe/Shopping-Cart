c48105713a194e94a5e5541094718086718f8250
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-08-09 20:29:22 -0300

    function fetchItem foi implementada e testada

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,14 @@
-const fetchItem = () => {
+const fetchItem = async (arg) => {
   // seu código aqui
+  const linkApi = `https://api.mercadolibre.com/sites/MLB/search?q=${arg}`;
+
+  const buscaApi = await fetch(linkApi);
+  const json = await buscaApi.json();
+  console.log(json);
+  return json;
 };
+
+fetchItem('MLB1615760527');
 
 if (typeof module !== 'undefined') {
   module.exports = {
