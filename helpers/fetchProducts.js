8675ebbbbcce31e3a3f9649f7511399c5495975e
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-08-09 19:51:47 -0300

    function fetchProducts foi implementada e testada

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,14 @@
-const fetchProducts = () => {
+const fetchProducts = async (arg) => {
   // seu código aqui
+  const linkApi = `https://api.mercadolibre.com/sites/MLB/search?q=${arg}`;
+  
+  const buscaApi = await fetch(linkApi);
+  const json = await buscaApi.json();
+  console.log(json);
+  return json;
 };
+
+fetchProducts('computador');
 
 if (typeof module !== 'undefined') {
   module.exports = {
